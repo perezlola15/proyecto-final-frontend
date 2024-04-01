8b3c4c09de7a803be398c566a805c5e6919d6a47
perezlola15 <lo.ruiz97@gmail.com>
2024-04-01 21:11:00 +0100

    add img and translate components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Login';
-import Cocina from './Components/Cocina';
-import Sala from './Components/Sala';
+import Kitchen from './Components/Kitchen';
+import Lounge from './Components/Lounge';
+import Admin from './Components/Admin';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/cocina" element={<Cocina />} />
-        <Route path="/sala" element={<Sala />} />
+        <Route path="/cocina" element={<Kitchen />} />
+        <Route path="/sala" element={<Lounge />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </Router>
   );
