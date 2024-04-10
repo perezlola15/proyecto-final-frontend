b01128ec498c814e7427002561d745363e6d102c
perezlola15 <lo.ruiz97@gmail.com>
2024-04-10 17:34:10 +0100

    add options

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Login from './Components/Login';
 import Kitchen from './Components/Kitchen';
 import Lounge from './Components/Lounge';
 import Admin from './Components/Admin';
+import Options from './Components/Options';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/options" element={<Options />} />
         <Route path="/cocina" element={<Kitchen />} />
         <Route path="/sala" element={<Lounge />} />
         <Route path="/admin" element={<Admin />} />
---
+++ src/Components/Options.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Style/Style.css';
+
+function Options() {
+    return (
+        <div>
+            <div className="navbar">
+                <img src="/img/logo.png" alt="Logo" />
+                <h1>PANEL DE ADMINISTRACIÓN</h1>
+            </div>
+            <div className="options-container">
+                <div className="options-lounge">
+                    <Link to="/lounge">
+                        <img src="/img/lounge.png" alt="Icono de sala" />
+                    </Link>
+                </div>
+                <div className="options-kitchen">
+                    <Link to="/kitchen">
+                        <img src="/img/kitchen.png" alt="Icono de cocina" />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Options;
