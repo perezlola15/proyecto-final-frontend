2b276c76b3f0103415c2583c0afe97f472013a9e
perezlola15 <lo.ruiz97@gmail.com>
2024-04-20 11:16:35 +0100

    Forms style staff

--- src/Components/staff/AddStaff.jsx
+++ src/Components/staff/AddStaff.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { MDBContainer, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 
 function AddStaff() {
   const [username, setUsername] = useState('');
@@ -29,49 +30,34 @@ function AddStaff() {
         <img src="/img/logo.png" alt="Logo" />
         <h1>PANEL DE ADMINISTRACIÓN</h1>
       </div>
-      <h2>Añadir staff</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Nombre de usuario:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="disabled">Desactivado:</label>
-          <input
-            type="checkbox"
-            id="disabled"
-            checked={disabled}
-            onChange={(e) => setDisabled(e.target.checked)}
-          />
-        </div>
-        <div>
-          <label htmlFor="locked">Bloqueado:</label>
-          <input
-            type="checkbox"
-            id="locked"
-            checked={locked}
-            onChange={(e) => setLocked(e.target.checked)}
-          />
-        </div>
-        <button type="submit">Crear usuario</button><br />
-        <Link to="/admin"><button className='button-create'>Volver atrás</button></Link>
-      </form>
+      <MDBContainer className="d-flex justify-content-center align-items-center vh-100">
+        <MDBCard style={{ width: '500px' }}>
+        <MDBCardBody className='p-5' style={{ marginTop: '-30px' }}>
+            <div className='d-flex flex-row justify-content-center align-items-center mb-4'>
+              <MDBIcon fas icon="cubes fa-3x me-3" />
+              <h5 className="fw-normal" style={{ letterSpacing: '1px' }}>Crear usuario</h5>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <MDBInput wrapperClass='mb-4' label='Usuario' id='formControlLg' type='text' size="lg" value={username} onChange={(e) => setUsername(e.target.value)} />
+              <MDBInput wrapperClass='mb-4' label='Contraseña' id='formControlLg' type='password' size="lg" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <div className='form-check mb-4'>
+                <input className='form-check-input' type='checkbox' id='disabled' checked={disabled} onChange={(e) => setDisabled(e.target.checked)} />
+                <label className='form-check-label' htmlFor='disabled'>Desactivado</label>
+              </div>
+              <div className='form-check mb-4'>
+                <input className='form-check-input' type='checkbox' id='locked' checked={locked} onChange={(e) => setLocked(e.target.checked)} />
+                <label className='form-check-label' htmlFor='locked'>Bloqueado</label>
+              </div>
+              <div className="d-flex justify-content-center">
+                <MDBBtn className="mb-4 custom-btn-width" color='dark' size='lg' type="submit" onClick={handleSubmit}>Crear</MDBBtn>
+              </div>
+            </form>
+            <div className="d-flex justify-content-center">
+              <Link to="/admin" className="mb-4 px-6 custom-btn-width btn btn-dark btn-lg">Volver atrás</Link>
+            </div>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
     </div>
   );
 }
--- src/Components/staff/UpdateStaff.jsx
+++ src/Components/staff/UpdateStaff.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
+import { MDBContainer, MDBCard, MDBCardBody, MDBInput, MDBBtn, MDBIcon } from 'mdb-react-ui-kit';
 import '../../style/Style.css';
 
 const UpdateStaff = () => {
@@ -22,7 +23,7 @@ const UpdateStaff = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.put('http://localhost:8082/project/api/staffs/${id}', staff);
+      await axios.put(`http://localhost:8082/project/api/staffs/${id}`, staff);
       // No hay redirección aquí, ya que se ha eliminado useHistory
     } catch (error) {
       console.error('Error updating staff:', error);
@@ -35,41 +36,36 @@ const UpdateStaff = () => {
         <img src="/img/logo.png" alt="Logo" />
         <h1>PANEL DE ADMINISTRACIÓN</h1>
       </div>
-      <h2>Editar staff</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={staff.username || ''}
-          onChange={(e) => setStaff({ ...staff, username: e.target.value })}
-        /><br /><br />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={staff.password || ''}
-          onChange={(e) => setStaff({ ...staff, password: e.target.value })}
-        /><br /><br />
-        <label htmlFor="disabled">Disabled:</label>
-        <input
-          type="checkbox"
-          id="disabled"
-          checked={staff.disabled || false}
-          onChange={(e) => setStaff({ ...staff, disabled: e.target.checked })}
-        /><br /><br />
-        <label htmlFor="locked">Locked:</label>
-        <input
-          type="checkbox"
-          id="locked"
-          checked={staff.locked || false}
-          onChange={(e) => setStaff({ ...staff, locked: e.target.checked })}
-        /><br /><br />
-        <button type="submit">Actualizar</button>
-        <Link to="/admin"><button className='button-create'>Volver atrás</button></Link>
-      </form>
+      <MDBContainer className="d-flex justify-content-center align-items-center vh-100">
+        <MDBCard style={{ width: '500px' }}>
+          <MDBCardBody className='p-5' style={{ marginTop: '-30px' }}>
+            <div className='d-flex flex-row justify-content-center align-items-center mb-4'>
+              <MDBIcon fas icon="cubes fa-3x me-3" />
+              <h5 className="fw-normal" style={{ letterSpacing: '1px' }}>Editar usuario</h5>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <MDBInput wrapperClass='mb-4' label='Usuario' id='formControlLg' type='text' size="lg" value={staff.username || ''} onChange={(e) => setStaff({ ...staff, username: e.target.value })} />
+              <MDBInput wrapperClass='mb-4' label='Contraseña' id='formControlLg' type='password' size="lg" value={staff.password || ''} onChange={(e) => setStaff({ ...staff, password: e.target.value })} />
+              <div className='form-check mb-4'>
+                <input className='form-check-input' type='checkbox' id='disabled' checked={staff.disabled || false} onChange={(e) => setStaff({ ...staff, disabled: e.target.checked })} />
+                <label className='form-check-label' htmlFor='disabled'>Desactivado</label>
+              </div>
+              <div className='form-check mb-4'>
+                <input className='form-check-input' type='checkbox' id='locked' checked={staff.locked || false} onChange={(e) => setStaff({ ...staff, locked: e.target.checked })} />
+                <label className='form-check-label' htmlFor='locked'>Bloqueado</label>
+              </div>
+              <div className="d-flex justify-content-center">
+                <MDBBtn className="mb-4 custom-btn-width" color='dark' size='lg' type="submit">Actualizar</MDBBtn>
+              </div>
+              <div className="d-flex justify-content-center">
+                <Link to="/admin" className="mb-4 px-6 custom-btn-width btn btn-dark btn-lg">Volver atrás</Link>
+              </div>
+            </form>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBContainer>
     </div>
   );
-};
+}
 
 export default UpdateStaff;
